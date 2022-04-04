981b6f71a1ab8210c007b17b757b3ea8ef84cc91
Rupali <rupalibh408@gmail.com>
2022-04-04 21:01:18 +0530

    project

---
+++ project/assets/js/click.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("module_Image_Click");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
